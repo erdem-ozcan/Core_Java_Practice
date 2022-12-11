42a5d30911fcdf7af3961855ebf30e7e07e4f2ea
erdem-ozcan <eozcan.uk@gmail.com>
2022-12-11 15:11:19 +0300

    switch

---
+++ src/p09_ternary_SwitchCase/Q11_SMConcat.java
@@ -0,0 +1,27 @@
+package p09_ternary_SwitchCase;
+
+public class Q11_SMConcat {
+
+    public static void main(String[] args) {
+
+        // String olarak verilen bir degisken + ile baska bir degiskenle isleme sokulursa
+        // jAVA bunlari yan yana ekler. Buna Concatenation denir.
+
+        String isim="Ali";
+        String soyisim="Can";
+
+        System.out.println(isim + " " + soyisim); // Ali Can
+
+        // ayni islemi + sembolu yerine concat() ile de yapabiliriz
+
+        System.out.println(isim.concat(soyisim));
+
+        System.out.println(isim.concat(" ").concat(soyisim)); // Ali Can
+
+
+
+
+
+
+    }
+}
