0d60ef1e813613ad82d94e1e353fafd1a685fdc5
erdem-ozcan <eozcan.uk@gmail.com>
2022-12-14 16:03:37 +0300

    s.m.

---
+++ src/p11_stringManipulations/Q05_Contains.java
@@ -0,0 +1,19 @@
+package p11_stringManipulations;
+
+public class Q05_Contains {
+
+    public static void main(String[] args) {
+
+        String cumle= "Calisirsaniz, java ogrenmek cok kolay";
+
+        System.out.println(cumle.contains(" ")); // true
+        System.out.println(cumle.contains("iz, j")); // true
+
+        System.out.println(cumle.contains("Hasan")); // false
+
+
+
+    }
+
+
+}
