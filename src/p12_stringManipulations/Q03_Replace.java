e0beb8c766fdb2b2beb7228f9ebc790e5c00a879
erdem-ozcan <eozcan.uk@gmail.com>
2022-12-15 14:41:10 +0300

    s.m.

---
+++ src/p12_stringManipulations/Q03_Replace.java
@@ -0,0 +1,17 @@
+package p12_stringManipulations;
+
+public class Q03_Replace {
+    public static void main(String[] args) {
+
+        String str = "Java ogrenmek cok zevkli";
+
+        System.out.println(str.replace(" ", "")); // Javaogrenmekcokzevkli
+
+        System.out.println(str.replace("ogrenmek", "kod yazmak"));
+
+        System.out.println(str.replace("e", "a"));
+
+
+
+    }
+}
